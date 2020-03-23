62b31c6a1b6c50f843ca2b100372b316906f386a
root <root@localhost.localdomain>
2020-03-23 15:19:49 +0000

    commit to add a frontpage

---
+++ precache-manifest.bdb1648c3a08086c176077753f711159.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727f3c06f3b528c830b1845317e41572",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad84be1e2679375e7bcb",
+    "url": "/static/css/main.64bf5cc5.chunk.css"
+  },
+  {
+    "revision": "1f31367b89aaaa112bd3",
+    "url": "/static/js/2.90c646db.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.90c646db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad84be1e2679375e7bcb",
+    "url": "/static/js/main.a200b75e.chunk.js"
+  },
+  {
+    "revision": "8743416c1497d06675c3",
+    "url": "/static/js/runtime-main.bb0d49c7.js"
+  },
+  {
+    "revision": "2cc4c3e758fee3cef83b08dc4a8f7f10",
+    "url": "/static/media/whatsapp.2cc4c3e7.svg"
+  }
+]);
