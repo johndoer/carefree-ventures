9e2d825e480ef13b6f64dcd4a7bee80d2556be97
root <root@localhost.localdomain>
2020-03-24 16:18:10 +0000

    add new front logos

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d5fd2d4e1703f68e644cfaa8d2735c9a.js"
+  "/precache-manifest.3c038cb24a2ef0e0c9616b3bfb6e095d.js"
 );
 
 self.addEventListener('message', (event) => {
