d39ad1de575ade4f3450385d455c347aeef01feb
root <root@localhost.localdomain>
2020-04-17 11:32:21 +0000

    pdfjs version branching for es5

---
+++ precache-manifest.140c2d609f0d7c079493986ab3755403.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2632350b61bdef8f24824fd604c8bd51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f66517adf44597158e6b",
+    "url": "/static/css/main.16465b23.chunk.css"
+  },
+  {
+    "revision": "8721d7ef4e715636631c",
+    "url": "/static/js/2.ffcb5a89.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ffcb5a89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f66517adf44597158e6b",
+    "url": "/static/js/main.eb49045f.chunk.js"
+  },
+  {
+    "revision": "8a430ee4d25e0e15133c",
+    "url": "/static/js/runtime-main.1c99f1bf.js"
+  },
+  {
+    "revision": "9b99bc15f33d90ba5e858b3260c8bca8",
+    "url": "/static/media/favicon.9b99bc15.ico"
+  },
+  {
+    "revision": "2cc4c3e758fee3cef83b08dc4a8f7f10",
+    "url": "/static/media/whatsapp.2cc4c3e7.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bc58d7d77e367223d5eaee59064760f9.js"
+  "/precache-manifest.140c2d609f0d7c079493986ab3755403.js"
 );
 
 self.addEventListener('message', (event) => {
